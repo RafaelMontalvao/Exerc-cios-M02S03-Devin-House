bbd7987f8d465681f0987a3de2df2db819d20cb5
Rafael Montalvão de Brito <113399882+RafaelMontalvao@users.noreply.github.com>
2023-02-07 08:35:47 -0300

    subindo o ex. 3
    commit ex 03 da semana 03

---
+++ ex03semana3.js
@@ -0,0 +1,7 @@
+let color = document.getElementById("color");
+let box1= document.getElementById("box1");
+console.log(box1.id);
+ function changeColor(){
+
+    box1.style.backgroundColor= color.value;
+ }
